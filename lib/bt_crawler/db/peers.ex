eb6a6e5085e22d6469e3c9ead6fb198e017643f4
Florian Adamsky <florian@adamsky.it>
2014-12-15 16:11:03 +0100

    Add validation to the Peers Model with a new unique function

--- lib/bt_crawler/db/peers.ex
+++ lib/bt_crawler/db/peers.ex
@@ -1,10 +1,29 @@
 defmodule BtCrawler.DB.Peers do
-	use Ecto.Model
+  use Ecto.Model
 
   schema "peers" do
-    # field :id, :integer, default: 0
     field :peer,      :string
     field :info_hash, :string
     field :requested, :integer
   end
+
+  validate peers,
+    peer: unique()
+
+  @doc """
+  This function checks if an entry is unique. It counts the number of
+  entries with a specific peer string. It returns {:ok} if it is
+  unique, or {:error} if it a peer already exists.
+  """
+  def unique(value, _opts \\ []) do
+    from(p in BtCrawler.DB.Peers,
+         where: p.peer == ^value,
+         select: count(p.id))
+    |> BtCrawler.DB.Repo.one
+    |> case do
+         0 -> {:ok}
+         _ -> {:error, "peer already exists"}
+       end
+  end
+
 end
