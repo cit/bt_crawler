d2e407cf6b7e4aafa5e6189df7f6d3f3d3f6d80d
Florian Adamsky <florian@adamsky.it>
2014-12-15 12:36:26 +0100

    Rewritten get_not_requested_peer which sets requested first
    Rewritten get_not_requested_peer which set requested first and then
    returns the peer element. This should work better when starting multiple processes.

--- lib/bt_crawler/db/query.ex
+++ lib/bt_crawler/db/query.ex
@@ -1,12 +1,25 @@
 defmodule BtCrawler.DB.Query do
   import Ecto.Query
+  alias  Ecto.Adapters.Postgres, as: Postgres
 
   def get_not_requested_peer do
-    query = from p in BtCrawler.DB.Peers,
-    where: p.requested == 0,
-    limit: 1,
-    select: p
-    BtCrawler.DB.Repo.all(query)
+    sql_query = """
+      UPDATE peers p
+      SET    requested=1
+      FROM (
+        SELECT peer
+        FROM   peers
+        WHERE  requested=0
+        LIMIT  1
+        FOR    UPDATE
+     ) sub
+     WHERE p.peer = sub.peer
+     RETURNING p.peer;
+    """
+
+    result = Postgres.query(BtCrawler.DB.Repo, sql_query, [])
+    %Postgrex.Result{rows: [rows]} = result
+    elem(rows, 0)
   end
 
   def get_peer(sock) when is_binary(sock) do
