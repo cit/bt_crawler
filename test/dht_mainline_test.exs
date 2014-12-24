a1610ef8d7f9c56fe490def8375c6616231aeaca
Florian Adamsky <florian@adamsky.it>
2014-12-24 08:02:22 +0100

    Added nicer comments

--- test/dht_mainline_test.exs
+++ test/dht_mainline_test.exs
@@ -3,14 +3,24 @@ defmodule BtCrawlerDHTMainlineTest do
 
   alias BtCrawler.DHT.Mainline
 
-  ## extract values
+  #####
+  ## Test parse() function
+  ##
+
+  ##  TODO
+
+  #####
+  ## Extract Values
+  ##
 
   test "extract_value works" do
     result = Mainline.extract_values([<<5, 13, 17, 155, 60, 197>>])
     assert result == [{"5.13.17.155", 15557}]
   end
 
-  ## extract nodes
+  #####
+  ## Extract Nodes
+  ##
 
   test "extract nodes works" do
     byte_str = <<250, 250, 250, 250, 250, 250, 250, 250, 250, 250,
@@ -25,21 +35,27 @@ defmodule BtCrawlerDHTMainlineTest do
     assert ip == {"46.19.115.68", 47622}
   end
 
+  #####
   ## PING Query
+  ##
 
   test "Mainline DHT query ping works" do
     str = Mainline.ping "aaaaaaaaaaaaaaaaaaaa"
     assert str == "d1:ad2:id20:aaaaaaaaaaaaaaaaaaaae1:q4:ping1:t2:aa1:y1:qe"
   end
 
+  #####
   ## FIND_NODE Query
+  ##
 
   test "Mainline DHT query find_node works" do
     str = Mainline.find_node "aaaaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbbbbbb"
     assert str == "d1:ad2:id20:aaaaaaaaaaaaaaaaaaaa6:target20:bbbbbbbbbbbbbbbbbbbbe1:q9:find_node1:t2:aa1:y1:qe"
   end
 
+  #####
   ## GET_PEERS Query
+  ##
 
   test "Mainline DHT query get_peers works" do
     str = Mainline.get_peers "aaaaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbbbbbb"
@@ -60,6 +76,5 @@ defmodule BtCrawlerDHTMainlineTest do
     str = Mainline.get_peers "aaaaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbbbbbb", "n6", :scrape
     assert str == "d1:ad2:id20:aaaaaaaaaaaaaaaaaaaa9:info_hash20:bbbbbbbbbbbbbbbbbbbb6:scrapei1e4:want2:n6e1:q9:get_peers1:t2:aa1:y1:qe"
   end
-
 
 end
