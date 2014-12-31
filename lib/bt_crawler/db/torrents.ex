49c78e7ce9970e33273ea516f8edfd2200aff3d8
cit <cit@haktar.org>
2014-12-31 15:25:54 +0100

    Fixed a typo in the table row requested_at

--- lib/bt_crawler/db/torrents.ex
+++ lib/bt_crawler/db/torrents.ex
@@ -2,9 +2,14 @@ defmodule BtCrawler.DB.Torrents do
   use Ecto.Model
 
   schema "torrents" do
+    field :piratebay_id,  :integer
+    field :name,          :string
+    field :size,          :integer
+    field :seeders,       :integer
+    field :leechers,      :integer
     field :info_hash,     :string
     field :requested,     :boolean, default: false
-    field :resquested_at, :datetime
+    field :requested_at,  :datetime
   end
 
 end
